00d0c8c1482ed2a058f78aa916bf20269d459677
alex937 <ava216@lehigh.edu>
2015-01-26 21:03:02 +0000

    added hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+//Alexander Arteaga
+//hw01
+//Welcome Class Java Program
+//
+//Like the first lab, compile the program with javac WelcomeClass.java
+// run the program java HelloWorld//
+
+// First define a class
+public class WelcomeClass{
+    // add main method
+      public static void main(String[] args) {
+          // Print the statement
+          System.out.println("      -----------            ");
+          System.out.println("      | WELCOME |            ");
+          System.out.println("    ^  ^  ^  ^  ^  ^         ");
+          System.out.println("   / \\/ \\/ \\/ \\/ \\/ \\  ");
+          System.out.println("  <-a--v--a--2--1--6->       ");
+          System.out.println("   \\ /\\ /\\ /\\ /\\ /\\ /  ");
+          System.out.println("    v  v  v  v  v  v         ");
+          System.out.println("Over winter break, I went skiing in the");
+          System.out.println("Apalachian Mountains. It was actually really fun.");
+          
+      }
+}
