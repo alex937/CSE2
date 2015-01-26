185a371e923de52e81df6f65afc245af69e4f2af
alex937 <ava216@lehigh.edu>
2015-01-26 20:12:15 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+//Alexander Arteaga
+//Lab01
+//Hello World Java Program
+//
+//First compile the program
+// javac HelloWorld.java
+// run the program
+// java HelloWorld//
+// define a class
+public class HelloWorld{
+    // add main method
+      public static void main(String[] args) {
+          // Print the statement with boxes around it
+          System.out.println("########################");
+          System.out.println("########################");
+          System.out.println("#                      #");
+          System.out.println("#   Hello World 2014   #");
+          System.out.println("#                      #");
+          System.out.println("########################");
+          System.out.println("########################");
+          System.out.println("test");
+          //test/testtest
+      }
+}
